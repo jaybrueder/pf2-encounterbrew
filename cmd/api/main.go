a3f4abdd7e9bb95d04cac5880bb5244078f5e577
Jürgen Brüder <juergen.brueder@gmail.com>
2024-10-04 20:06:28 +0200

    feat: added Redis Cache

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"encoding/gob"
 
 	"pf2.encounterbrew.com/internal/server"
+	"pf2.encounterbrew.com/internal/models"
 )
 
+func init() {
+    gob.Register(&models.Encounter{})
+    gob.Register(&models.Player{})
+    gob.Register(&models.Monster{})
+    gob.Register(map[string]interface{}{})
+}
+
 func main() {
-	server := server.NewServer()
-	err := server.ListenAndServe()
+	server, err := server.NewServer()
+	if err != nil {
+		panic(fmt.Sprintf("cannot initialize server: %s", err))
+	}
+
+	err = server.ListenAndServe()
 
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
