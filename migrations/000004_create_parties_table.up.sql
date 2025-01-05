eb36efa4e4b3561ba9a8e123e356b0f91e557873
Jürgen Brüder <juergen.brueder@gmail.com>
2025-01-05 17:17:04 +0100

    feat: add fort, will and ref to players (#59)

--- migrations/000004_create_parties_table.up.sql
+++ migrations/000004_create_parties_table.up.sql
@@ -3,14 +3,3 @@ CREATE TABLE IF NOT EXISTS parties (
     name VARCHAR(255) NOT NULL,
     user_id INTEGER REFERENCES users(id)
 );
-
--- Add default seeds for development purposes
-INSERT INTO parties (name, user_id)
-VALUES ('Gold', (SELECT id FROM users WHERE name = 'Default' ORDER BY id LIMIT 1))
-ON CONFLICT (id) DO NOTHING
-RETURNING id;
-
-INSERT INTO parties (name, user_id)
-VALUES ('Red', (SELECT id FROM users WHERE name = 'Default' ORDER BY id LIMIT 1))
-ON CONFLICT (id) DO NOTHING
-RETURNING id;
